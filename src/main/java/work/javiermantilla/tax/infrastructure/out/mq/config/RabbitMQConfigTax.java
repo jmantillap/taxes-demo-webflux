f450897dad9055e5ba53062e87ef09c3dd7d8277
Javier Orlando Mantilla Portilla <jomantil@bancolombia.com.co>
2025-03-10 16:46:02 -0500

    feat: Se Ajusta con la anotacion @primary para que cree la cola si no existe y no presente problemas cuando se suscriba a la cola en la clase ReactiveRabbitMQConsumerHandler

--- src/main/java/work/javiermantilla/tax/infrastructure/out/mq/config/RabbitMQConfigTax.java
+++ src/main/java/work/javiermantilla/tax/infrastructure/out/mq/config/RabbitMQConfigTax.java
@@ -1,5 +1,6 @@
 package work.javiermantilla.tax.infrastructure.out.mq.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -7,15 +8,22 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import work.javiermantilla.tax.infrastructure.in.event.config.ReactiveRabbitMQConfig;
+
 
 @Configuration
+@Primary
 public class RabbitMQConfigTax {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ReactiveRabbitMQConfig.class.getName());
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Primary
     public Queue queue(@Value("${rabbitmq.queue-name}") final String queueName) {
+        LOGGER.info("📌 Creando cola de RabbitMQ antes de iniciar la aplicación..." +queueName);
         return new Queue(queueName, true);
     }
 
